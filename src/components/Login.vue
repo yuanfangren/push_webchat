6ca935581e63465011d11a29bec452acb11f9553
weiw <weiw@opera.com>
2019-06-02 11:08:44 +0800

    add channel substribe list

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,18 +7,26 @@
 		<div class="content">
 			{{content}}
 		</div>
+		<div>
+			<img :src="imgurl" alt="">
+		</div>
 	</div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: 'login',
   data () {
     return {
       msg: '欢迎回来',
-      content:"这是登录&注册内容"
+			content:"这是登录&注册内容",
+			imgurl: ''
     }
-  }
+	},
+	methods: {
+		
+	}
 }
 </script>
 
